c1fdc2544979a686b3c704f09de076bfb23b6d52
lugino-emeritus <lugino-emeritus@users.noreply.github.com>
2022-02-01 17:46:41 +0100

    new reload method

--- ntlib/imp.py
+++ ntlib/imp.py
@@ -1,9 +1,10 @@
 """Provide import tools and can set a basic logging format."""
 
+import importlib as _il
+import os
 import sys
-from importlib import import_module, reload
 
-__version__ = '0.2.4'
+__version__ = '0.2.6'
 
 _confpath = None
 _aliases = None
@@ -34,15 +35,20 @@ def load_config(name):
 	with open(_confpath) as f:
 		return _confread(f)[name]
 
+#-------------------------------------------------------
 
-def import_path(modulename, path=''):
-	org = sys.path[:]
-	try:
-		if path:
-			sys.path.insert(0, path)
-		return import_module(modulename)
-	finally:
-		sys.path[:] = org
+class _EnsureSysPath:
+	def __init__(self, path):
+		self.path = os.path.abspath(path)
+	def __enter__(self):
+		sys.path.insert(0, self.path)
+	def __exit__(self, *exc_args):
+		sys.path.remove(self.path)
+
+
+def import_path(modulename, path='.'):
+	with _EnsureSysPath(path):
+		return _il.import_module(modulename)
 
 def import_alias(alias, modulename):
 	"""import module from an alias path defined in the config.json file."""
@@ -53,3 +59,19 @@ def import_alias(alias, modulename):
 	if tail:
 		modulename = '.'.join((tail, modulename))
 	return import_path(modulename, path)
+
+def reload(module):
+	if not module.__file__.endswith('.py'):
+		raise ImportError(f"module '{module.__name__}' is no '.py' file: {module.__file__}")
+	path, tail = os.path.split(module.__file__[:-3])
+	if tail == '__init__':
+		path, tail = os.path.split(path)
+	names = module.__name__.split('.')
+	while names.pop() == tail:
+		if not names:
+			break
+		path, tail = os.path.split(path)
+	else:
+		raise ImportError(f"module '{module.__name__}' not match source: {module.__file__}")
+	with _EnsureSysPath(path):
+		return _il.reload(module)
