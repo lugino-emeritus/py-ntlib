27beb6653be720f457c0f380e5ee63f286c16973
lugino-emeritus <lugino-emeritus@users.noreply.github.com>
2022-02-27 11:59:03 +0100

    update _confpath.py

--- ntlib/_confpath.py
+++ ntlib/_confpath.py
@@ -3,13 +3,13 @@
 import os
 from sys import platform
 
-root = os.path.abspath(os.path.split(__file__)[0])
-# if the confpath is platform independent
-#confpath = os.path.join(root, 'config.json')
+root = os.path.dirname(__file__)
+# if the confpath is platform independent:
+# confpath = os.path.join(root, 'config.json')
 
 if platform.startswith('linux'):
-	confpath = os.path.join(root, 'config/linux.json')
+	confpath = os.path.join(root, 'config', 'linux.json')
 elif platform.startswith('win'):
-	confpath = os.path.join(root, 'config/windows.json')
+	confpath = os.path.join(root, 'config', 'windows.json')
 else:
 	raise ImportError(f'confpath not available for platform {platform}')
