7f18722f128351ba004094a7381763ba1687269b
lugino-emeritus <nik_t.01@web.de>
2022-03-26 15:08:01 +0100

    generic update

---
+++ git-push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+set -e
+cd ${0%/*}
+sudo mount -B /media/nti/Data/000/Impl/python/lib/ntlib/ ntlib/
+cp ntlib/README.md README.md
+git add .
+git status
+git commit -m "$1"
+sudo unmount ntlib/
